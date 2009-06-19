3073326658d2834393d827b4b3dedaae3abf5c14
Luis Lavena <luislavena@gmail.com>
2009-06-19 17:04:03 -0300

    Added commit parsing and applying.

--- lib/tasks/cherry.rake
+++ lib/tasks/cherry.rake
@@ -1,6 +1,29 @@
+class GitHelper
+  def find_by_tickets(*tickets)
+    `git log --abbrev-commit --pretty=oneline master | grep -E "#{tickets.join('|')}"`
+  end
+
+  def apply_commit(sha)
+
+  end
+end
+
 task :cherry, :tickets do |t, args|
   args.with_defaults(:tickets => "")
   tickets = args.tickets
+  helper = GitHelper.new
 
   fail "You must supply at least one ticket number" if tickets.empty?
+
+  output = helper.find_by_tickets(tickets)
+
+  # build commit list
+  commits = output.scan(/([a-f0-9]{7}).*?\s(.*)/i)
+
+  commits.each do |sha, message|
+    puts "Applying commit #{sha} of Ticket ##{tickets} (#{message})"
+    unless result = helper.apply_commit(sha)
+      fail "ERROR: Cannot apply commit #{sha}, aborting"
+    end
+  end
 end
--- spec/lib/tasks/cherry_spec.rb
+++ spec/lib/tasks/cherry_spec.rb
@@ -11,9 +11,46 @@ describe "cherry task" do
     end
   end
 
+  context 'with a supplied ticket' do
+    before :each do
+      @helper = mock(GitHelper, :null_object => true)
+      @helper.stub!(:find_by_tickets).and_return(mocked_commits)
+
+      GitHelper.stub!(:new).and_return(@helper)
+    end
+
+    it 'should find the relevant commits from master' do
+      out, _ = run_task('426')
+      out.should match(/Applying commit f665dcd/)
+    end
+
+    it 'should apply relevant commits in order' do
+      @helper.should_receive(:apply_commit).once.ordered.with("f665dcd").and_return(true)
+      @helper.should_receive(:apply_commit).once.ordered.with("8a65b36").and_return(true)
+      @helper.should_receive(:apply_commit).once.ordered.with("cea9613").and_return(true)
+      run_task('426')
+    end
+
+    it 'should abort if a commit between fails' do
+      @helper.should_receive(:apply_commit).once.with("f665dcd").and_return(false)
+      @helper.should_not_receive(:apply_commit).with("8a65b36")
+      lambda { run_task('426') }.should raise_error(RuntimeError, /Cannot apply commit f665dcd, aborting/)
+    end
+  end
+
   private
   def run_task(arg = nil)
-    Rake::Task['cherry'].reenable
-    Rake::Task['cherry'].invoke(arg)
+    capture_output do
+      Rake::Task['cherry'].reenable
+      Rake::Task['cherry'].invoke(arg)
+    end
+  end
+
+  def mocked_commits
+    <<-EOF
+f665dcd Ticket #426: Fixing something
+8a65b36 Ticket #426
+cea9613 Ticket #426
+EOF
   end
 end
